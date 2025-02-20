82bc2bf04e96e41e5874b3f64d26a5e0630e7481
Andrea <132267442+andreahauberg@users.noreply.github.com>
2025-02-20 12:29:28 +0100

    modal and dialog

--- js/contact.js
+++ js/contact.js
@@ -5,16 +5,34 @@ document.querySelector("#frmContact").addEventListener("submit", (e) => {
   const email = e.target.txtEmail.value;
   const comments = e.target.txtComments.value;
 
-  const submittedInfo = `
-  Thank You for Your message. We will get back to you soon!
-  
-  Registered information:
-  - Name: ${name}
-  - Email: ${email}
-  - Comments: ${comments}
-  `;
+  const modal = document.querySelector("#contact_message");
 
-  alert(submittedInfo);
+  modal.querySelector("#name").innerText = name;
+  modal.querySelector("#email").innerText = email;
+  modal.querySelector("#comments").innerText = comments;
 
-  window.location.replace("index.html");
+  modal.showModal();
+  modal.focus();
+
+  // modal.querySelector(".close").addEventListener("click", function () {
+  //   this.parentElement.parentElement.close();
+  // });
+  modal.querySelector(".close").addEventListener("click", () => {
+    modal.close();
+  });
+
+  modal.addEventListener("close", () => {
+    window.location.replace("index.html");
+  });
 });
+
+// const submittedInfo = `
+// Thank You for Your message. We will get back to you soon!
+
+// Registered information:
+// - Name: ${name}
+// - Email: ${email}
+// - Comments: ${comments}
+// `;
+
+// alert(submittedInfo);
